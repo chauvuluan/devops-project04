ac6e43800c927b6e65eed396398b8c1249468eeb
luancv <chauvuluan@gmail.com>
2023-03-19 22:28:17 +0700

    p4

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=luancv/project04
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project04 --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward project04 8080:80
